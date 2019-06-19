ced569a14ac79057ae79520b044d3ecabf8c91ff
Raya Hazi <44358915+rayahazi@users.noreply.github.com>
2019-06-19 08:58:01 +0300

    app

--- src/FinalProjectHouseCommittiee/Server/Manager.java
+++ src/FinalProjectHouseCommittiee/Server/Manager.java
@@ -14,6 +14,7 @@ import java.util.Vector;
 /*
  * Class Manager - 
  * will handle user of type - manager 
+ * 1. Menu - options to choose
  * 
  */
 
@@ -34,47 +35,45 @@ public class Manager extends User {
 	 *  0 for exit
 	 * */
 	@Override
-    public void menu() throws IOException {
-        while (true){
-            try {
-            	// Output to client: see options in menu
-        		System.out.println("\nManager:\n-------------------");        	
-        		System.out.println("--------Menu: --------");
-        		System.out.println("[1] Ternant payment plan(per months)");
-        		System.out.println("[2] Payment of all ternants in our building");
-        		System.out.println("[3] Update payment details for ternant");
-        		System.out.println("[4] Monthly income");
-        		System.out.println("[0] exit");
-        		
-                int res = inFromClient.read();
-                switch (res) {
-                    case 1:
-                    	TernantPayment();
-                        break;
-                    case 2:
-                    	AllPayments();
-                        break;
-                    case 3:
-                    	DeletePay();
-                        break;
-                    case 4:
-                        UpdatePay();
-                        break;                  
-                    case 0:
-                        return;
-
-                }
-            }catch (SQLException | ClassNotFoundException e) {
-                e.printStackTrace();
-            }
+        public void menu() throws IOException {
+	    while (true){
+                try {
+		// Output to client: see options in menu
+		System.out.println("\nManager:\n-------------------");        	
+		System.out.println("--------Menu: --------");
+		System.out.println("[1] Ternant payment plan(per months)");
+		System.out.println("[2] Payment of all ternants in our building");
+		System.out.println("[3] Update payment details for ternant");
+		System.out.println("[4] Monthly income");
+		System.out.println("[0] exit");
+		
+	        // Switch-case for selected choice by user manager
+		int res = inFromClient.read();
+		switch (res) {
+		    case 1:
+			TernantPayment();
+			break;
+		    case 2:
+			AllPayments();
+			break;
+		    case 3:
+			DeletePay();
+			break;
+		    case 4:
+			UpdatePay();
+			break;                  
+		    case 0:
+			return;
+		}
+	    }catch (SQLException | ClassNotFoundException e) {
+		e.printStackTrace();
+	    }
         }
     }
 
 	// case 1: specific ternant's payment:
     private void TernantPayment() throws IOException, SQLException {
-        String apartmentNum = inFromClient.readLine();
-
-        
+        String apartmentNum = inFromClient.readLine();      
         PreparedStatement check_if_exist = sql.connect().prepareStatement("select FirstName,LastName,monthPay from ternant where ApartmentId= ?");
         check_if_exist.setString(1, apartmentNum);
         ResultSet check_result = check_if_exist.executeQuery();
@@ -84,8 +83,7 @@ public class Manager extends User {
             outToClient.writeBytes("Wrong parameter!" + "\n");
             outToClient.writeBytes("end" + "\n");
             return;
-        }
-
+	}
         // If exists:
         PreparedStatement statement = sql.connect().prepareStatement("select FirstName,LastName,monthPay from ternant where ApartmentId= ?");
         statement.setString(1, apartmentNum);
@@ -105,34 +103,18 @@ public class Manager extends User {
     }
 
 	
-    // case 2: 
+    // case 2 - function to show all payments of ternants: 
     private void AllPayments() throws SQLException, IOException {
     	String queryString = "select ApartmentId,monthPay from ternant";			
 		String answerString = sql.select_query(1, queryString);
         outToClient.writeBytes(answerString.toString() + "\n");
         outToClient.writeBytes("end" + "\n");
     }
-
-    
-    private void display_income() throws SQLException, IOException {
-        /* This function display income from db*/
-        for (Integer i = 2; i < 14; i++) {
-            String str = "SELECT SUM(m" + String.valueOf(i-1) + ") FROM paidmonth";
-            PreparedStatement statement = sql.connect().prepareStatement(str);
-            ResultSet result = statement.executeQuery();
-            result.next();
-            String sum = result.getString(1);
-            /* Send back to the client tha total income payments for month */
-            outToClient.writeBytes("Total income for month " +  String.valueOf(i-1) + ": " + sum + "\n");
-        }
-        outToClient.writeBytes("ok" + "\n");
-    }
-
-    
+	
     
     // Delete function: 
     private void DeletePay() throws SQLException, IOException {
-        /* This function delete payment from db*/
+        // This function delete payment from db
         String apartmentNum = inFromClient.readLine();
         PreparedStatement statement = sql.connect().prepareStatement("SELECT apartmentNum FROM resident WHERE apartmentNum = ?");
         statement.setString(1, apartmentNum);
@@ -154,14 +136,13 @@ public class Manager extends User {
     }
     
     
-    
+    // Update function:
     private void UpdatePay() throws IOException, SQLException, ClassNotFoundException {
         //This function update payment in db
-        String str[] = inFromClient.readLine().split(" "); //apartmentNum, payment, month
-
+	// divide to apartmentNum, payment, month
+        String str[] = inFromClient.readLine().split(" "); 
         System.out.println("apartmentNum " + str[0] + "payment " + str[1] + "month " + str[2]);
-        PreparedStatement statement = sql.connect().prepareStatement(
-                "select * from ternant where apartmentNum = ?");
+        PreparedStatement statement = sql.connect().prepareStatement("select * from ternant where apartmentNum = ?");
         statement.setString(1, str[0]);
         ResultSet result = statement.executeQuery();
 
@@ -178,7 +159,7 @@ public class Manager extends User {
         updateStatement.setString(2, str[0]);
         updateStatement.executeUpdate();
 
-        /* Send back to the client that payment as update */
+        // Send back to the client that payment as update 
         outToClient.writeBytes("Payment updated for ternant\n");
         outToClient.writeBytes("end\n");
     }
